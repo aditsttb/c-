8d8f0b81c6a5df3418cbe8829567b7b6601d7c96
aditsttb <aditya.sttb@gmail.com>
2017-03-12 12:31:40 +0700

    Add files via upload
    identitas diri

---
+++ identitas.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+	using namespace std;
+int main ( ) {
+	char name [50];
+	int age;
+	char gender [50];
+	char religion [50];
+	
+	
+		cout<<" Please enter your name:  ";
+			cin>> name;
+				cout <<" Your name is: " << name << endl;
+				
+		cout<<" Please enter your age:  ";
+			cin>> age;
+				cout <<" Your age is: " << age << endl;
+				
+		cout<<" Please enter your gender:  ";
+			cin>> gender;
+				cout <<" Your gender is: " << gender << endl;
+				
+		cout<<" Please enter your Religion:  ";
+			cin>> religion;
+				cout <<" Your religion is: " << religion << endl;
+		
+		return 0;
+		}
