297816ea2af86ceba3472a4e140154b4969c35d5
Aditya permana <aditya.sttb@gmail.com>
2017-04-10 23:07:07 +0700

    tugas max
    menentukan tugas max 10.April.2017

---
+++ max.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int a, b;
+
+int main () 
+{
+	cout<<"APLIKASI MENENTUKAN NILAI MAX"<<endl;
+	//membandingkan dua buah nilai
+	cout<<"masukan nilai a :  "; cin>>a;
+	cout<<"masukan nilai b :  "; cin>>b;
+	
+	
+	if (a<b) {
+	cout<<" nilai max = "<<b;
+	
+	}
+		else 
+			{ cout<<" nilai max= "<<a;
+			  
+		
+			}
+	
+	return 0;
+	
+}
+	
